e1c4131114659e360f2e1661ea1c5ce87125a0a5
Guhan <bkguhan2001@gmail.com>
2020-04-15 11:06:14 +0530

    "guhan porfolio page v1"

---
+++ contact.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Primary Meta Tags -->
+	<title>Guhan</title>
+	<meta name="title" content="Guhan">
+	<meta name="description" content="Hey! Have  look at my portfolio and résumé.">
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="http://guhanportfolio.000webhostapp.com/">
+	<meta property="og:title" content="Guhan">
+	<meta property="og:description" content="Hey! Have  look at my portfolio and résumé.">
+	<meta property="og:image" content="img/guhan.png">
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image">
+	<meta property="twitter:url" content="http://guhanportfolio.000webhostapp.com/">
+	<meta property="twitter:title" content="Guhan">
+	<meta property="twitter:description" content="Hey! Have  look at my portfolio and résumé.">
+	<meta property="twitter:image" content="img/guhan.png">
+
+	<!--===========font section==============-->
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c:500,700&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Source+Code+Pro:300,400,500,700&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Dancing+Script:400,500,600,700&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lily+Script+One&display=swap" rel="stylesheet">
+
+	<!--===========tab image==============-->
+	<link rel="icon" href="img/favico.png">
+
+	<!--===========main css section==============-->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!--===========media css section==============-->
+	<link rel="stylesheet" href="css/media.css">
+
+</head>
+
+<body>
+	
+	<!--====================== navber part start ====================-->
+
+	<nav class="navbar">
+		<div class="sidebar-btn">
+			<div class="nav-right">
+				<div class="button" id="btn">
+					<svg viewBox="0 0 800 600">
+						<path d="M300,220 C300,220 520,220 540,220 C740,220 640,540 520,420 C440,340 300,200 300,200" id="top"></path>
+						<path d="M300,320 L540,320" id="middle"></path>
+						<path d="M300,210 C300,210 520,210 540,210 C740,210 640,530 520,410 C440,330 300,190 300,190" id="bottom" transform="translate(480, 320) scale(1, -1) translate(-480, -318) "></path>
+					</svg>
+				</div>
+			</div>
+		</div>
+		<div class="sidebar">
+			<ul class="sidebar-list">
+				<li class="sidebar-item"><a href="index.html" class="sidebar-anchor">Home</a></li>
+				<li class="sidebar-item"><a href="about.html" class="sidebar-anchor">About</a></li>
+				<li class="sidebar-item"><a href="contact.php" class="sidebar-anchor current">Contact</a></li>
+				<li class="sidebar-item"><a href="projects.html" class="sidebar-anchor">Projects</a></li>
+				<li class="sidebar-item"><a href="pdf/sampleresume.pdf" target="_blank" class="sidebar-anchor">Résumé</a></li>
+			</ul>
+		</div>
+		
+	</nav>
+	<!--====================== navber part ends ====================-->
+	
+	<section id="home">
+		<div id="brand"><a href="index.html">Guhan</a></div>
+
+		<div class="contact-text">
+			<div class="container-lg">
+				<div class="top-section">
+					<h1>Guhan here, Wanna talk?</h1>
+					<p>
+						Do you want to have a casual chat? or Do you have any ideas for projects/collaboration? or Do you have any advice for improving my skills?
+						Yes, sure I'm excited to have a conversation with you, feel free to get in touch. You can contact me on any one of the social media below or you can mail me.
+					</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="contact-2">
+			<div class="container-lg">
+				<div class="bottom-contact">
+					<img src="img/avatar.png" title="me but just animified">
+					<p>I am currently living in Tamil Nadu, India. I promise to reply to as many messages as possible. I am looking forward to hearing from you.</p>
+					
+				</div>
+			</div>
+		</div>
+		<div class="form-section">
+			<div class="container-lg">
+				<div class="form-section-content">
+					<div class="fcnt">
+						<div>
+							<h1>Leave a message here.</h1> 
+							<h1>or</h1> 
+							<h1>Contact me through any of my social links below.</h1>
+						</div>
+					</div>
+					<div class="form">
+						<form action="" method="POST">
+							<input type="email" name="email" id="email" placeholder="Email" required autocomplete="off">
+							<input type="text" name="name" id="name" placeholder="Name" required autocomplete="off">
+							<textarea name="message" id="message" placeholder="Message" required autocomplete="off"></textarea>	
+							<input type="submit" value="SUBMIT" name="submit" class="btn-submit">					
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="social-media">
+			<div class="container-lg">
+				<div class="sm-list">
+					<div id="instagram" class="sm">
+						<i class="fab fa-instagram-square"></i>
+						<a href="https://www.instagram.com/_mr_knowhere_/" class="sm-link" target="_blank">Instagram</a>
+						<p>A place filled with memories and memes.</p>
+					</div>
+					<div id="twitter" class="sm">
+						<i class="fab fa-twitter-square"></i>
+						<a href="https://twitter.com/Guhan63704794" class="sm-link" target="_blank">Twitter</a>
+						<p>I usually don't tweet and if i do it's not me.</p>
+					</div>
+					<div id="facebook" class="sm">
+						<i class="fab fa-facebook-square"></i>
+						<a href="https://www.facebook.com/" class="sm-link" target="_blank">Facebook</a>
+						<p>Ahh! I wonder why do I have an account here.</p>
+					</div>
+					<div id="mail" class="sm">
+						<i class="fab fa-google-plus-square"></i>
+						<a href="mailto:bkguhan2001@gmail.com?Subject=Hello" class="sm-link" target="_blank">Mail</a>
+						<p>Can anyone tell me how to get my inbox to zero?</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- <div class="container-lg"><hr></div> -->
+
+		<footer >
+			<div class="footer-content container-lg">
+				<div class="left">
+					<p>&copy; 2020 Guhan</p>
+				</div>
+				<div class="right">
+					<p><a href="https://www.instagram.com/_mr_knowhere_/" target="_blank">Instagram</a> : <a href="https://www.facebook.com/" target="_blank">Facebook</a> : <a href="https://twitter.com/Guhan63704794" target="_blank">Twitter</a> : <a href="mailto:bkguhan2001@gmail.com?Subject=Hello" target="_blank">Gmail</a> : <a href="#">Résumé</a></p>
+				</div>
+			</div>
+		</footer>
+	</section>	
+
+	<!--============jquery js section===========-->
+	<script src="js/jquery-1.12.4.min.js"></script>
+
+	<!--============custom js section===========-->
+	<script src="js/custom.js"></script>
+
+	<!--=========fontawsome cdn section=========-->
+	<script src="https://kit.fontawesome.com/ffce93f859.js" crossorigin="anonymous"></script>
+
+	<!-- ========PHP mail script=============== -->
+	<?php
+		if(isset($_POST['submit']))
+		{
+			if(!empty($_POST['email']) && !empty($_POST['name']) && !empty($_POST['message']))
+			{
+				$to = "bkguhan2001@gmail.com";
+				$from = $_POST['email'];
+				$name = $_POST['name'];
+				$message = $_POST['message'];
+				$subject = "Contact mail from name:$name through your portfolio page.";
+				$header = "From:$from";
+				if(mail($to,$subject,$message,$header))
+				{
+					echo "success";
+				}
+				else{
+					echo "failed";
+				}
+			}
+		}
+
+	?>
+
+
+</body>
+
+</html>
